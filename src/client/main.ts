33e33fb342fa002985acbe9f0d90c2789195f262
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-08-08 19:41:07 +0800

    auto save editor width

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
 const input = document.querySelector('#editor textarea') as HTMLTextAreaElement
 
 input.value = localStorage.getItem('input') || input.value
+input.style.width = localStorage.getItem('input_width') || ''
 
 window.addEventListener('storage', () => {
   input.value = localStorage.getItem('input') || input.value
@@ -9,3 +10,11 @@ window.addEventListener('storage', () => {
 input.addEventListener('input', () => {
   localStorage.setItem('input', input.value)
 })
+
+try {
+  new MutationObserver(() => {
+    localStorage.setItem('input_width', input.style.width)
+  }).observe(input, { attributes: true })
+} catch (error) {
+  console.error('MutationObserver not supported')
+}
