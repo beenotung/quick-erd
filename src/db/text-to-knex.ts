b5aeeb60adc60eb3a6951ce70de273570650dac3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-10-15 00:11:42 +0800

    fix migrate order bug when resolving reference dependency

--- src/db/text-to-knex.ts
+++ src/db/text-to-knex.ts
@@ -19,23 +19,25 @@ export async function up(knex: Knex): Promise<void> {
 `
 
       const table_list = result.table_list
-      table_list.slice().forEach(self => {
-        self.field_list.forEach(field => {
-          const ref = field.references
-          if (!ref) return
+      for (let i = 0; i < table_list.length; i++) {
+        table_list.slice().forEach(self => {
+          self.field_list.forEach(field => {
+            const ref = field.references
+            if (!ref) return
 
-          const other = table_list.find(table => table.name === ref.table)
-          if (!other) return
+            const other = table_list.find(table => table.name === ref.table)
+            if (!other) return
 
-          const selfIndex = table_list.indexOf(self)
-          const otherIndex = table_list.indexOf(other)
+            const selfIndex = table_list.indexOf(self)
+            const otherIndex = table_list.indexOf(other)
 
-          if (otherIndex <= selfIndex) return
+            if (otherIndex <= selfIndex) return
 
-          table_list[otherIndex] = self
-          table_list[selfIndex] = other
+            table_list[otherIndex] = self
+            table_list[selfIndex] = other
+          })
         })
-      })
+      }
 
       table_list.forEach(table => {
         code += `
