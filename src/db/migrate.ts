1e58dfd6b3d64e3fab6396033d874a6d67fe5e95
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-03-24 23:59:24 +0800

    remove migrate.ts from src

--- src/db/migrate.ts
+++
@@ -1,9 +0,0 @@
-import { knex } from './knex'
-import { down, up } from '../../migrate'
-
-down(knex)
-  .then(() => up(knex))
-  .catch(e => {
-    console.error(e)
-  })
-  .finally(() => knex.destroy())
