70b8d09be65fa043bdd2d91b8a6712b882633799
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-05-24 17:25:00 +0800

    support pg and mysql clients for auto-migrate cli

--- src/cli/auto-migrate.ts
+++ src/cli/auto-migrate.ts
@@ -2,7 +2,6 @@
 
 import { parse } from '../core/ast'
 import { readErdFromStdin } from '../utils/file'
-import { dbFile } from '../db/sqlite'
 import {
   detectSrcDir,
   setupKnexFile,
@@ -13,14 +12,31 @@ import { loadKnex } from '../db/knex'
 
 /* eslint-disable no-console */
 
+const dbFile_or_client = process.argv[2]
+if (!dbFile_or_client) {
+  console.error('missing database client or sqlite filename in argument')
+  process.exit(1)
+}
+
 async function main() {
   const erd = await new Promise<string>(resolve => readErdFromStdin(resolve))
   const parseResult = parse(erd)
   const srcDir = detectSrcDir()
-  setupSqlite({ srcDir, dbFile })
-  const db_client = 'better-sqlite3'
+  let db_client: string
+  switch (dbFile_or_client) {
+    case 'mysql':
+    case 'pg':
+    case 'postgresql':
+      db_client = dbFile_or_client
+      break
+    default: {
+      db_client = 'better-sqlite3'
+      const dbFile = dbFile_or_client
+      setupSqlite({ srcDir, dbFile })
+    }
+  }
   setupKnexFile({ srcDir, db_client })
-  const knex = loadKnex()
+  const knex = loadKnex(db_client)
   await setupKnexMigration({ knex, parseResult, db_client })
 }
 
