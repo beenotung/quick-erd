5635add657bdb4248cf3b577bb9472801bb52fa4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-06-25 17:48:56 +0800

    revamp query builder to avoid using path with non-selected field to join table

--- src/core/query-2.ts
+++ src/core/query-2.ts
@@ -7,65 +7,185 @@ export function generateQuery(
   tableList: ParseResult['table_list'],
 ) {
   const graph = new Schema.Graph(tableList)
-  const plan = new Query.Plan(columns, graph)
+  const select = new Query.Select(columns, graph)
   return {
-    sql: plan.toSQL(),
+    sql: SQL.fromSelect(select),
   }
 }
 
 namespace Query {
-  export class Plan {
-    selectFields: SelectField[] = []
+  export class Select {
+    selectFieldNodes: Schema.FieldNode[]
     fromTableNode?: Schema.TableNode
-    connectedTables = new Map<string, ConnectedTable>()
+    joinTables = new Map<Schema.TableNode, Join>()
+    tableAliases = new Map<Schema.TableNode, Set<string>>()
     constructor(columns: Column[], public graph: Schema.Graph) {
-      columns.forEach(column => {
-        this.selectColumn(column)
-      })
+      this.selectFieldNodes = columns.map(column =>
+        graph.getTable(column.table).getField(column.field),
+      )
+      this.findConnections()
     }
-    selectColumn(column: Column) {
-      const field = new SelectField(column, this.graph)
-      this.selectFields.push(field)
-
-      const tableNode = this.graph.getTable(column.table)
-
+    findConnections() {
+      const pendingFieldNodes = new Set(this.selectFieldNodes)
+      for (; pendingFieldNodes.size > 0; ) {
+        const oldCount = pendingFieldNodes.size
+        pendingFieldNodes.forEach(fieldNode => {
+          if (this.findConnection(fieldNode)) {
+            pendingFieldNodes.delete(fieldNode)
+          }
+        })
+        const newCount = pendingFieldNodes.size
+        if (oldCount != newCount) continue
+        throw new DisconnectedError(Array.from(pendingFieldNodes))
+      }
+    }
+    findConnection(selectFieldNode: Schema.FieldNode): boolean {
+      const selectTableNode = selectFieldNode.tableNode
       if (!this.fromTableNode) {
-        this.fromTableNode = tableNode
-        this.connectedTables.set(column.table, new ConnectedTable(tableNode))
-        return
+        this.fromTableNode = selectTableNode
+        return true
+      }
+      if (this.fromTableNode == selectTableNode) return true
+      if (this.joinTables.has(selectTableNode)) return true
+
+      // reference by connected table
+      for (const [
+        referenceByFieldNode,
+        referenceBy,
+      ] of selectTableNode.referenceByFields) {
+        if (!this.selectFieldNodes.includes(referenceByFieldNode)) {
+          continue
+        }
+        if (
+          referenceByFieldNode.tableNode == this.fromTableNode ||
+          this.joinTables.has(referenceByFieldNode.tableNode)
+        ) {
+          const join: Join = {
+            leftFieldNode: referenceBy.fromFieldNode,
+            rightFieldNode: referenceBy.toFieldNode,
+          }
+          this.joinTables.set(selectTableNode, join)
+          if (referenceBy.alias) {
+            this.addTableAlias(selectTableNode, referenceBy.alias)
+          }
+          return true
+        }
       }
 
-      if (this.connectedTables.has(column.table)) return
+      const referenceTo = selectFieldNode.reference
+      if (!referenceTo) return false
+
+      // reference to connected table
+      if (
+        referenceTo.toFieldNode.tableNode == this.fromTableNode ||
+        this.joinTables.has(referenceTo.toFieldNode.tableNode)
+      ) {
+        const join: Join = {
+          leftFieldNode: referenceTo.toFieldNode,
+          rightFieldNode: referenceTo.fromFieldNode,
+        }
+        this.joinTables.set(selectTableNode, join)
+        if (referenceTo.alias) {
+          this.addTableAlias(
+            referenceTo.fromFieldNode.tableNode,
+            referenceTo.alias,
+          )
+        }
+        return true
+      }
+
+      return false
     }
-    toSQL(): string {
-      let sql = `select`
-      this.selectFields.forEach(field => {
-        sql +=
-          '\n, ' + field.tableNode.table.name + '.' + field.fieldNode.field.name
-      })
-      // first select column don't need to start with comma
-      sql = sql.replace(', ', '  ')
-      sql += '\nfrom ' + this.selectFields[0].tableNode.table.name
-      return sql
+    addTableAlias(tableNode: Schema.TableNode, alias: string) {
+      const aliases = this.tableAliases.get(tableNode)
+      if (aliases) {
+        aliases.add(alias)
+      } else {
+        this.tableAliases.set(tableNode, new Set([alias]))
+      }
+    }
+  }
+  export class DisconnectedError extends Error {
+    constructor(public pendingFieldNodes: Schema.FieldNode[]) {
+      super(
+        'failed to connect some tables: ' +
+          Array.from(
+            new Set(
+              pendingFieldNodes.map(
+                fieldNode => fieldNode.tableNode.table.name,
+              ),
+            ),
+          ).join(', '),
+      )
     }
   }
   export class SelectField {
-    tableNode: Schema.TableNode
-    fieldNode: Schema.FieldNode
     tsType: string
-    constructor(column: Column, graph: Schema.Graph) {
-      this.tableNode = graph.getTable(column.table)
-      this.fieldNode = this.tableNode.getField(column.field)
+    alias?: string
+    constructor(public fieldNode: Schema.FieldNode) {
       this.tsType = toTsType(this.fieldNode.field.type)
       if (this.fieldNode.field.is_null) {
         this.tsType = 'null | ' + this.tsType
       }
     }
   }
-  export class ConnectedTable {
-    alias?: string
-    constructor(public tableNode: Schema.TableNode) {}
+
+  export interface Join {
+    leftFieldNode: Schema.FieldNode
+    rightFieldNode: Schema.FieldNode
   }
+}
+
+namespace SQL {
+  export function fromSelect(select: Query.Select) {
+    let sql = `select`
+    select.selectFieldNodes.forEach(fieldNode => {
+      forEachAlias(select.tableAliases.get(fieldNode.tableNode), tableAlias => {
+        const tableName = tableAlias || fieldNode.tableNode.table.name
+        sql += '\n, ' + tableName + '.' + fieldNode.field.name
+      })
+    })
+    // first select column don't need to start with comma
+    sql = sql.replace(', ', '  ')
+    if (!select.fromTableNode) {
+      throw new Error('missing from table')
+    }
+    sql += '\nfrom ' + select.fromTableNode.table.name
+    for (const join of select.joinTables.values()) {
+      sql += fromJoin(select, join)
+    }
+    return sql
+  }
+  export function fromJoin(select: Query.Select, join: Query.Join): string {
+    const rightTableNode = join.rightFieldNode.tableNode
+    const leftTableNode = join.leftFieldNode.tableNode
+    let sql = ''
+    forEachAlias(select.tableAliases.get(rightTableNode), rightTableAlias => {
+      forEachAlias(select.tableAliases.get(leftTableNode), leftTableAlias => {
+        let rightTableName = rightTableNode.table.name
+        sql += '\ninner join ' + rightTableName
+        if (rightTableAlias) {
+          rightTableName = rightTableAlias
+          sql += ' as ' + rightTableName
+        }
+        sql += ' on ' + rightTableName + '.' + join.rightFieldNode.field.name
+        const leftTableName = leftTableAlias || leftTableNode.table.name
+        sql += ' = ' + leftTableName + '.' + join.leftFieldNode.field.name
+      })
+    })
+    return sql
+  }
+}
+
+function forEachAlias(
+  aliases: Set<string> | undefined,
+  eachFn: (alias: string | null) => void,
+) {
+  if (!aliases || aliases.size === 0) {
+    eachFn(null)
+    return
+  }
+  aliases.forEach(alias => eachFn(alias))
 }
 
 namespace Schema {
@@ -73,8 +193,9 @@ namespace Schema {
     private tableNodes: Map<string, TableNode>
     constructor(table_list: Table[]) {
       this.tableNodes = new Map(
-        table_list.map(table => [table.name, new TableNode(table)]),
+        table_list.map(table => [table.name, new TableNode(this, table)]),
       )
+      this.tableNodes.forEach(table => table.applyReferences())
     }
     getTable(name: string) {
       const table = this.tableNodes.get(name)
@@ -85,10 +206,17 @@ namespace Schema {
 
   export class TableNode {
     private fieldNodes: Map<string, FieldNode>
-    constructor(public table: Table) {
+    referenceByFields = new Map<FieldNode, Reference>()
+    constructor(public graph: Graph, public table: Table) {
       this.fieldNodes = new Map(
-        table.field_list.map(field => [field.name, new FieldNode(field, this)]),
+        table.field_list.map(field => [
+          field.name,
+          new FieldNode(graph, this, field),
+        ]),
       )
+    }
+    applyReferences() {
+      this.fieldNodes.forEach(field => field.applyReference())
     }
     getField(name: string) {
       const field = this.fieldNodes.get(name)
@@ -98,6 +226,30 @@ namespace Schema {
   }
 
   export class FieldNode {
-    constructor(public field: Field, public tableNode: TableNode) {}
+    reference?: Reference
+    constructor(
+      public graph: Graph,
+      public tableNode: TableNode,
+      public field: Field,
+    ) {}
+    applyReference() {
+      const reference = this.field.references
+      if (!reference) return
+      const refTable = this.graph.getTable(reference.table)
+      const refField = refTable.getField(reference.field)
+      const alias = this.field.name.replace(/_id$/, '')
+      this.reference = {
+        fromFieldNode: this,
+        toFieldNode: refField,
+        alias: alias == refField.tableNode.table.name ? null : alias,
+      }
+      refTable.referenceByFields.set(this, this.reference)
+    }
+  }
+
+  export interface Reference {
+    fromFieldNode: FieldNode
+    toFieldNode: FieldNode
+    alias: string | null
   }
 }
