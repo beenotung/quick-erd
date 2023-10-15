c468d1b4635025d7d286195748d15b5e2166ea6f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-10-15 23:31:51 +0800

    chore: remove unused import

--- examples/java-project/src/main/java/com/example/javaproject/entity/UserEntity.java
+++ examples/java-project/src/main/java/com/example/javaproject/entity/UserEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
--- src/db/text-to-spring.ts
+++ src/db/text-to-spring.ts
@@ -134,7 +134,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 ${importLines}
 `.trim()
