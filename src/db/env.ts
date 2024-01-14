dc29e75255a980a86830e26f5b49229ab3518916
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-14 13:58:29 +0800

    patch: make usage on dotenv optional

--- src/db/env.ts
+++ src/db/env.ts
@@ -1,5 +1,8 @@
-import { config } from 'dotenv'
-
-config()
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config()
+} catch (error) {
+  // dotenv is not installed, it is optional
+}
 
 export const env = process.env
